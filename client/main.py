ece5e9e22ce26e848a3d2eac6d39699768e846bb
jryzkns <jryzkns@gmail.com>
2021-12-18 22:36:37 -0800

    moves pygame init and window handle to main

--- client/main.py
+++ client/main.py
@@ -3,20 +3,28 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import zmq
 import uuid
 
+import pygame as pg
+
 from definitions import *
 from gameSession import GameSession
 from connection import Connection
 
+pg.init()
+game_win = pg.display.set_mode(res)
+
 with zmq.Context() as zmq_ctx:
 
     should_stop = not SHOULD_STOP_GAME
+
     while not should_stop:
 
         player_uuid = uuid.uuid4().hex
 
         Connection.connect(zmq_ctx, player_uuid)
 
-        should_stop = GameSession.run_session(zmq_ctx, player_uuid)
+        pg.display.set_caption(f'Playing as: {player_uuid}')
+
+        should_stop = GameSession.run_session(zmq_ctx, game_win, player_uuid)
 
         if should_stop:
             Connection.disconnect(zmq_ctx, player_uuid)
