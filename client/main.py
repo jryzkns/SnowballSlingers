60d0c61548c3c2d2c2e9907db12cfa8281d46062
Melanie Xu <xu.melanie@yahoo.ca>
2021-12-26 14:10:58 -0800

    implemented end page

--- client/main.py
+++ client/main.py
@@ -4,6 +4,7 @@ import zmq
 import uuid
 import pygame as pg
 
+from end import endScreen
 from definitions import *
 from gameSession import GameSession
 from connection import Connection
@@ -13,21 +14,18 @@ pg.init()
 game_win = pg.display.set_mode(res)
 
 with zmq.Context() as zmq_ctx:
-
-    address = loginPage(game_win)
+    rejoin = False
     should_stop = not SHOULD_STOP_GAME
-
     while not should_stop:
-
+        if not rejoin:
+            address = loginPage(game_win)
         player_uuid = uuid.uuid4().hex
-
         Connection.connect(zmq_ctx, player_uuid, address)
-
         pg.display.set_caption(f'Playing as: {player_uuid}')
-
         should_stop = GameSession.run_session(zmq_ctx, game_win, player_uuid, address)
-
         if should_stop:
             Connection.disconnect(zmq_ctx, player_uuid, address)
-
+        if not should_stop:
+            rejoin = endScreen(game_win)
+            should_stop = rejoin is None
     zmq_ctx.destroy(1000)
