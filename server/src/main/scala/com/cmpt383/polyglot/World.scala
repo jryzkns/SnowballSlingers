6e9183713dfdc9b1e3d997129d25476d58f44661
Jack Zhou <jryzkns@gmail.com>
2022-05-01 14:08:21 -0700

    formatting changes

--- server/src/main/scala/com/cmpt383/polyglot/World.scala
+++ server/src/main/scala/com/cmpt383/polyglot/World.scala
@@ -28,10 +28,10 @@ class World {
     def deregister_player ( id : String ) : Unit = {
         if ( _players.contains( id ) ) {
             _players -= id
-            _updates.enqueue(s"$id-disconn")
-            println(s"Player $id have left the game")
+            _updates.enqueue( s"$id-disconn" )
+            println( s"Player $id have left the game" )
         } else {
-            println(s"Player $id was not here to begin with")
+            println( s"Player $id was not here to begin with" )
         }
     }
 
@@ -56,22 +56,26 @@ class World {
         }
     }
 
-    def get_uuid() : String = java.util.UUID.randomUUID.toString.replace("-", "")
+    def get_uuid() : String = java.util.UUID.randomUUID.toString.replace( "-", "" )
 
     def process_incoming( msg : String ) : Unit = {
         msg match {
-            case msg_regex(player, body) => {
-                if (!is_registered(player)) { return }
+            case msg_regex( player, body ) => {
+                if ( !is_registered( player ) ) {
+                    return
+                }
                 body match {
-                    case cast_regex(x, y) =>
+                    case cast_regex( x, y ) =>
                         var caster = _players( player )
-                        if (caster.attempt_cast()) {
-                            _updates.enqueue(s"$player-ack")
+                        if ( caster.attempt_cast() ) {
+                            _updates.enqueue( s"$player-ack" )
                             _snowballs += new Snowball( get_uuid(),
-                                player, caster.x, caster.y, (x.toInt, y.toInt))
+                                                        player,
+                                                        caster.x, caster.y,
+                                                        ( x.toInt, y.toInt ) )
                         }
-                    case click_regex(x, y) =>
-                        _players(player).intent = Some((x.toInt, y.toInt))
+                    case click_regex( x, y ) =>
+                        _players( player ).intent = Some( ( x.toInt, y.toInt ) )
                 }
             }
             case _ => return
@@ -80,56 +84,57 @@ class World {
 
     def tick() : Unit = {
         val now = System.currentTimeMillis()
-        val dt = (now - _refresh_time)/1000.0
-        _players foreach ( { case (_, player) => player.tick(dt)})
+        val dt = ( now - _refresh_time ) / 1000.0
+        _players foreach ( { case ( _, player ) => player.tick( dt ) } )
         _snowballs foreach ( snowball => {
-            snowball.tick(dt)
+            snowball.tick( dt )
             val this_caster = snowball.caster
             // check if this snowball collides with any players
-            _players foreach ( { case (p_name, player) => {
-                val dist = hypot(player.x - snowball.x, player.y - snowball.y)
-                if ( this_caster != p_name && dist < Definitions._snowball_player_collide_dist) {
+            _players foreach ( { case ( p_name, player ) => {
+                val dist = hypot( player.x - snowball.x,
+                                  player.y - snowball.y )
+                if ( this_caster != p_name && dist < Definitions._snowball_player_collide_dist ) {
                     snowball.lifetime = 0
-                    player.hp = math.max(0, player.hp - 1)
-                    _updates.enqueue(s"$p_name-hit")
+                    player.hp = math.max( 0, player.hp - 1 )
+                    _updates.enqueue( s"$p_name-hit" )
                 }
-            }})
-            // check if this snowbal lcollides with any other snowballs
+            } } )
+            // check if this snowball collides with any other snowballs
             _snowballs foreach ( other_snowball => {
-                if (this_caster != other_snowball.caster && other_snowball.lifetime != 0) {
-                    val dist = hypot(   other_snowball.x - snowball.x,
-                                        other_snowball.y - snowball.y)
-                    if (dist < Definitions._snowball_snowball_collide_dist) {
+                if ( this_caster != other_snowball.caster && other_snowball.lifetime != 0 ) {
+                    val dist = hypot( other_snowball.x - snowball.x,
+                                      other_snowball.y - snowball.y )
+                    if ( dist < Definitions._snowball_snowball_collide_dist ) {
                         snowball.lifetime = 0
                         other_snowball.lifetime = 0
                     }
                 }
 
-            })
-        })
+            } )
+        } )
 
-        _snowballs.filter(_.lifetime == 0) foreach {
-            snowball => _updates.enqueue(s"${snowball.uuid}-d")
+        _snowballs.filter( _.lifetime == 0 ) foreach {
+            snowball => _updates.enqueue( s"${snowball.uuid}-d" )
         }
 
-        _snowballs = _snowballs.filterNot(_.lifetime == 0)
+        _snowballs = _snowballs.filterNot( _.lifetime == 0 )
 
-        _players foreach ( { case (p_name, player) => {
-            if (player.hp == 0) {
-                deregister_player(p_name)
+        _players foreach ( { case ( p_name, player ) => {
+            if ( player.hp == 0 ) {
+                deregister_player( p_name )
             }
-        }})
+        } } )
 
         _refresh_time = now
     }
 
-    def tick_updates() : java.util.List[String] = {
-        var out_updates = List[String]()
-        out_updates = _updates.dequeueAll(_ => true).toList ::: out_updates
-        out_updates = (for ((id, player) <- _players) yield
+    def tick_updates() : java.util.List[ String ] = {
+        var out_updates = List[ String ]()
+        out_updates = _updates.dequeueAll( _ => true ).toList ::: out_updates
+        out_updates = ( for ( ( id, player ) <- _players ) yield
                         s"${id}-${player.name}-(${player.x.toInt}, ${player.y.toInt})"
                         ).toList ::: out_updates
-        out_updates = (for (snowball <- _snowballs) yield
+        out_updates = ( for ( snowball <- _snowballs ) yield
                         s"${snowball.uuid}-(${snowball.x.toInt}, ${snowball.y.toInt})"
                         ).toList ::: out_updates
         out_updates.asJava
