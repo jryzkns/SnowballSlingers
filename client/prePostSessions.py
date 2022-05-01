a91a40df0cc74c175dd4e422f2e38ce40d999efc
Jack Zhou <jryzkns@gmail.com>
2022-05-01 00:02:23 -0700

    UI: improvements
     - once the input is valid, can press enter to begin session
     - text alignment is now improved

--- client/prePostSessions.py
+++ client/prePostSessions.py
@@ -9,13 +9,13 @@ from widgets import Button, TextBox, TextMultiline
 def loginPage( screen, cm ):
     address_regex = re.compile( r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|localhost)$" )
     input_box = TextBox( 50, 80, 200, 32, address_regex )
+    input_box.active = True
     button = Button( 90, 30, "Connect", ( 50, 130 ), start_active=False )
     text = TextMultiline( [ "Welcome to Snowball Slingers.",
                             "Please enter the server IP address into the text box below.",
                             "Click the Connect button after you have entered the IP address",
                           ], 20, 10 )
 
-    dest = None
     should_stop = False
     while not should_stop:
         for event in pg.event.get():
@@ -24,9 +24,10 @@ def loginPage( screen, cm ):
             elif event.type == pg.MOUSEBUTTONDOWN:
                 input_box.on_mousebuttondown( event.pos )
                 if button.on_mousebuttondown( event.pos ):
-                    dest = input_box.text_input
                     should_stop = True
             elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_RETURN and input_box.valid:
+                    should_stop = True
                 if input_box.active:
                     input_box.on_keydown( event.key )
                     button.active = input_box.valid
@@ -36,9 +37,8 @@ def loginPage( screen, cm ):
         button.draw( screen )
         pg.display.flip()
 
-    # TODO: make this not a blocking operation
     player_uuid = uuid.uuid4().hex
-    cm.do_connect( dest, player_uuid )
-    pg.display.set_caption( f'{dest} : {player_uuid}' )
+    cm.do_connect( input_box.text_input, player_uuid )
+    pg.display.set_caption( f'{input_box.text_input} : {player_uuid}' )
 
     return 0
--- client/utils.py
+++ client/utils.py
@@ -1,3 +1,6 @@
 def colorstr2triple( color_str ):
     r, g, b = color_str[ :2 ], color_str[ 2:4 ], color_str[ 4: ]
     return ( int( r, 16 ), int( g, 16 ), int( b, 16 ) )
+
+def translate( x, y, dx, dy ):
+    return x + dx, y + dy
